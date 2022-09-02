00cf72e753aea56cd9a749b22bc8c8bda7ab85db
hezaz <62071557+hezaz@users.noreply.github.com>
2022-09-02 23:41:34 +0200

    v++

--- iamrich/lib/main.dart
+++ iamrich/lib/main.dart
@@ -3,9 +3,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Early Eyes',
+            style: TextStyle(
+              color: Color.fromRGBO(18, 122, 196, 1),
+            ),
+          ),
+          backgroundColor: Color.fromRGBO(185, 247, 77, 1),
+        ),
+        backgroundColor: Color.fromRGBO(185, 247, 77, 1),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+            ),
+        ),
+        ),
       ),
-    ),
   );
 }
